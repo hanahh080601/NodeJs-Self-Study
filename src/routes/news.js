60e5a3d7db55f29efe9ecf1613ffde79d3675a57
hanahh080601 <hanlehngoc080601@gmail.com>
2021-11-06 22:39:07 +0700

    edit sth and install JSON viewer extension

--- src/routes/news.js
+++ src/routes/news.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const NewsController = require('../app/controllers/NewsController');
 
-router.use('/:slug', NewsController.show);
-router.use('/', NewsController.index);
+router.get('/:slug', NewsController.show);
+router.get('/', NewsController.index);
 
 module.exports = router;
