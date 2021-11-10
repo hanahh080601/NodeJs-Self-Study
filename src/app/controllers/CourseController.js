1175b75f1ae7ea67b09d3f0c0616be3579af850e
hanahh080601 <hanlehngoc080601@gmail.com>
2021-11-10 23:30:46 +0700

    Select all with checkbox

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,12 +1,18 @@
 const Course = require('../models/Course');
-const { mongooseToObject } = require('../../util/mongoose');
+const {
+    mongooseToObject
+} = require('../../util/mongoose');
 
 class CourseController {
     // [GET] /courses/:slug
     show(req, res, next) {
-        Course.findOne({slug: req.params.slug})
+        Course.findOne({
+                slug: req.params.slug
+            })
             .then(course => {
-                res.render('courses/show', {course: mongooseToObject(course)});
+                res.render('courses/show', {
+                    course: mongooseToObject(course)
+                });
             })
             .catch(next);
     }
@@ -38,30 +44,55 @@ class CourseController {
 
     // [PUT] /courses/:id
     update(req, res, next) {
-        Course.updateOne({_id: req.params.id}, req.body)
+        Course.updateOne({
+                _id: req.params.id
+            }, req.body)
             .then(() => res.redirect('/me/stored/courses'))
             .catch(next);
     }
 
     // [DELETE] /courses/:id
     delete(req, res, next) {
-        Course.delete({_id: req.params.id})
+        Course.delete({
+                _id: req.params.id
+            })
             .then(() => res.redirect('back'))
             .catch(next);
     }
 
     // [DELETE] /courses/:id/force
     forceDelete(req, res, next) {
-        Course.deleteOne({_id: req.params.id})
+        Course.deleteOne({
+                _id: req.params.id
+            })
             .then(() => res.redirect('back'))
             .catch(next);
     }
 
     // [PATCH] /courses/:id/restore
     restore(req, res, next) {
-        Course.restore({_id: req.params.id})
+        Course.restore({
+                _id: req.params.id
+            })
             .then(() => res.redirect('back'))
             .catch(next);
+    }
+
+    // [POST] /courses/handle-form-actions
+    handleFormActions(req, res, next) {
+        switch (req.body.action) {
+            case 'delete':
+                Course.delete({
+                        _id: {$in: req.body.courseIds} 
+                    })
+                    .then(() => res.redirect('back'))
+                    .catch(next);
+                break;
+            default:
+                res.json({
+                    message: 'Action is invalid!'
+                });
+        }
     }
 
 }
